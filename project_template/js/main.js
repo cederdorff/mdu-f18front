7284a96d983ee42eab7d308d005beee4a7b249cc
Rasmus Cederdorff <rasmus@cederdorff.com>
2019-01-09 08:43:06 +0100

    Initials projects added.

---
+++ project_template/js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// appends string to the DOM
+document.querySelector("#page-content").innerHTML = "Hi Frontenders!";
+
+// declaring techer objects
+let teacher1 = {
+  name: 'Birgitte',
+  initials: 'bki'
+};
+
+let teacher2 = {
+  name: 'Michael',
+  initials: 'mhv'
+};
+
+let teacher3 = {
+  name: 'Rasmus',
+  initials: 'race'
+};
+
+// logs the teacher objects to the console
+console.log(teacher1);
+console.log(teacher2);
+console.log(teacher3);
