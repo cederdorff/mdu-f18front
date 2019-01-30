fab70a701561554429c8c7ef0ffe32bf8ffd9bfa
Rasmus Cederdorff <rasmus@cederdorff.com>
2019-01-30 08:39:21 +0100

    object_teachers_grid solutiuon added.

--- object_teachers_grid/js/main.js
+++ object_teachers_grid/js/main.js
@@ -1,10 +1,8 @@
 "use strict";
 
-// appends a string to the DOM
-document.querySelector("#page-content").innerHTML = "Hi Frontenders!";
-
 // declaring techer objects
 
+// Birgitte
 let teacher1 = {
   name: "Birgitte Kirk Iversen",
   initials: "bki",
@@ -16,21 +14,38 @@ let teacher1 = {
   img: "https://www.baaa.dk/CropUp/headshot/media/1524902/birgitte-kirk-iversen.jpg"
 };
 
+// Michael
 let teacher2 = {
-
+  name: "Michael Hvidtfeldt",
+  initials: "mhv",
+  mail: "mhv@baaa.dk",
+  phone: "72286328",
+  address: "Sønderhøj 30, 8260 Viby J",
+  position: "Senior Lecturer",
+  department: "Multimedia Design",
+  img: "https://www.eaaa.dk/CropUp/headshot/media/116737/Michael-Hvidtfeldt.jpg"
 };
 
+// Rasmus
 let teacher3 = {
-
+  name: "Rasmus Cederdorff",
+  initials: "mhv",
+  mail: "race@baaa.dk",
+  phone: "72286318",
+  address: "Sønderhøj 30, 8260 Viby J",
+  position: "Lecturer",
+  department: "Multimediedesigner & Professionsbachelor i digital konceptudvikling",
+  img: "http://eaaa.cederdorff.com/mdu-f18front/object_teachers_grid/img/race.jpg"
 };
 
-// logs the teacher objects to the console
+// log objects to the developer console
 console.log(teacher1);
 console.log(teacher2);
 console.log(teacher3);
 
-// append data fromn the objects to the document
-// teacher1
+// Appending objects to the DOM
+
+// teacher1 - Birgitte
 document.querySelector("#grid-teachers").innerHTML +=
   "<article>" +
   "<img src='" + teacher1.img + "'>" +
@@ -39,6 +54,20 @@ document.querySelector("#grid-teachers").innerHTML +=
   "<a href='mailto:" + teacher1.mail + "'>" + teacher1.mail + "</a>" +
   "<article>";
 
-//teacher2  
+//teacher2 - Michael
+document.querySelector("#grid-teachers").innerHTML +=
+  "<article>" +
+  "<img src='" + teacher2.img + "'>" +
+  "<h3>" + teacher2.name + "</h3>" +
+  teacher2.position + "<br>" +
+  "<a href='mailto:" + teacher2.mail + "'>" + teacher2.mail + "</a>" +
+  "<article>";
 
-//teacher3
+//teacher3 - Rasmus
+document.querySelector("#grid-teachers").innerHTML +=
+  "<article>" +
+  "<img src='" + teacher3.img + "'>" +
+  "<h3>" + teacher3.name + "</h3>" +
+  teacher3.position + "<br>" +
+  "<a href='mailto:" + teacher3.mail + "'>" + teacher3.mail + "</a>" +
+  "<article>";
