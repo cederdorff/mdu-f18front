3b7b2bafd5577ea240e57a818680cb8d75057d18
Rasmus Cederdorff <rasmus@cederdorff.com>
2019-02-15 08:22:43 +0100

    Clear functionality added.

--- web_storage_bootstrap/js/main.js
+++ web_storage_bootstrap/js/main.js
@@ -1,9 +1,19 @@
 "use strict";
 
+function firstTimeUser() {
+  let firstTimeUser = localStorage.getItem("firstTimeUser");
+  console.log("firstTimeUser", firstTimeUser);
+  if (firstTimeUser === null) {
+    $("#first-time-user-modal").modal("show");
+    localStorage.setItem("firstTimeUser", false);
+  }
+}
+
+firstTimeUser();
+
 function saveLocalStorage() {
   let email = document.querySelector("#local-storage-email").value;
   console.log(email);
-
   // store value in local storage
   localStorage.setItem("email", email);
   // call loadFromStorage to update displayed values
@@ -29,12 +39,17 @@ function loadFromStorage() {
   console.log("sessionSavedEmail", sessionSavedEmail);
 
   // set input field with email values from storage
-  document.querySelector('#local-storage-email').value = localStorageEmail;
-  document.querySelector('#session-storage-email').value = sessionSavedEmail;
+  document.querySelector("#local-storage-email").value = localStorageEmail;
+  document.querySelector("#session-storage-email").value = sessionSavedEmail;
 
   // set span tags with email values from storage
-  document.querySelector('#local-saved-email').innerHTML = localStorageEmail || "no data";
-  document.querySelector('#session-saved-email').innerHTML = sessionSavedEmail || "no data";
+  document.querySelector("#local-saved-email").innerHTML = localStorageEmail || "no data";
+  document.querySelector("#session-saved-email").innerHTML = sessionSavedEmail || "no data";
 }
 
 loadFromStorage();
+
+function clearStorage() {
+  localStorage.clear();
+  sessionStorage.clear();
+}
