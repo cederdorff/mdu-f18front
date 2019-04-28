78816cac977ee0d60035f286800c09e871ca6a89
Rasmus Cederdorff <rasmus@cederdorff.com>
2019-04-28 13:20:19 +0200

    CRUD materialize starter template

---
+++ crud_materializecss_starter/js/main.js
@@ -0,0 +1,66 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function() {
+  init();
+});
+
+let posts = [{
+  title: "Hola",
+  content: "This is my awesome content",
+  image: "http://cederdorff.com/img/cphcloud_web.jpg"
+}];
+
+function init() {
+  // modal
+  let elems = document.querySelectorAll('.modal');
+  let instances = M.Modal.init(elems);
+
+  readPosts();
+}
+
+// ------ CRUD operations ------ //
+
+function createPost() {
+
+}
+
+function readPosts() {
+  let htmlTemplate = "";
+  for (let post of posts) {
+    console.log(post);
+    htmlTemplate += `
+    <div class="col s12 m6">
+      <article class="card">
+        <div class="card-image">
+          <img src="${post.image}">
+          <span class="card-title">${post.title}</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">more_horiz</i></a>
+        </div>
+        <div class="card-content">
+          <p>${post.content}</p>
+        </div>
+      </article>
+    </div>
+    `;
+  }
+  document.querySelector('#postFeed').innerHTML = htmlTemplate;
+}
+
+function updatePost() {
+
+}
+
+function deletePost() {
+
+}
+
+// ------ Prieview image function ------ //
+function previewImage(file) {
+  if (file) {
+    let reader = new FileReader();
+    reader.onload = function(event) {
+      document.querySelector('#imagePreview').setAttribute('src', event.target.result);
+    };
+    reader.readAsDataURL(file);
+  }
+}
